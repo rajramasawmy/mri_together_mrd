be19224e59610459ba74ed25262c1ba6be02d050
rajramasawmy <raj_ramasawmy@hotmail.com>
2021-12-07 22:44:43 -0500

    data less than 50MB, update shell, remove large files

--- shell/convert_siemens_data.sh
+++ shell/convert_siemens_data.sh
@@ -1,2 +1,7 @@
 #!/bin/sh 
-siemens_to_ismrmrd -f ../data/siemens_cardiac_ssfp_crt_snapshot.dat -z 2 -o ../data/converted_siemens_data.h5
+
+# convert the experiment
+siemens_to_ismrmrd -f ../data/siemens_raw_cartesian.dat -z 2 -o ../data/converted_siemens_data.h5
+
+# convert the included noise dependencies (for pre-whitening)
+siemens_to_ismrmrd -f ../data/siemens_raw_cartesian.dat -z 2 -o ../data/converted_siemens_data.h5
